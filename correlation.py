e64077851ad1e07e265843d0a2e54609c8e37fea
artmerinov <artemmerinov@gmail.com>
2023-10-18 17:38:32 +0200

    refactoring

--- correlation.py
+++ correlation.py
@@ -2,70 +2,72 @@ import numpy as np
 
 
 def pearson_correlation(x, y):
+    """
+    # Calculate the Pearson correlation coefficient (r).
+    """
+    mu_x = np.mean(x)
+    mu_y = np.mean(y)
 
-    # Calculate the mean of x and y
-    mean_x = sum(x) / len(x)
-    mean_y = sum(y) / len(y)
+    cov_xy = np.sum((x - mu_x) * (y - mu_y))
+    sd_x = np.sqrt(np.sum((x - mu_x) ** 2))
+    sd_y = np.sqrt(np.sum((y - mu_y) ** 2))
 
-    # Calculate the differences from the mean for both variables
-    diff_x = [xi - mean_x for xi in x]
-    diff_y = [yi - mean_y for yi in y]
-
-    # Calculate the sum of squared differences
-    sum_squared_diff_x = sum([(xi - mean_x) ** 2 for xi in x])
-    sum_squared_diff_y = sum([(yi - mean_y) ** 2 for yi in y])
-
-    # Calculate the sum of the product of differences
-    sum_product_diff = sum(diff_x[i] * diff_y[i] for i in range(len(x)))
-
-    # Calculate the Pearson correlation coefficient (r)
-    r = sum_product_diff / (pow(sum_squared_diff_x, 0.5) * pow(sum_squared_diff_y, 0.5))
-
+    r = cov_xy / (sd_x * sd_y)
+    
     return r
 
 
-def spearman_correlation(x, y):
-    
-    # Create ranked versions of x and y
-    rank_x = [sorted(x).index(xi) + 1 for xi in x]
-    rank_y = [sorted(y).index(yi) + 1 for yi in y]
+def rank(arr):
+    """
+    Assigns ranks to the elements of an array in ascending order.
+    """
+    sorted_ids = np.argsort(arr)
 
+    ranks = np.zeros(len(arr))
+    ranks[sorted_ids] = np.arange(1, len(arr) + 1)
+
+    return ranks
+
+
+def spearman_correlation(x, y):
+    """
+    Calculate the Spearman correlation coefficient (rho).
+    """
+    # calculate the differences between ranks
+    d = rank(x) - rank(y)
     n = len(x)
 
-    # Calculate the differences between ranks
-    rank_diff = [rank_x[i] - rank_y[i] for i in range(n)]
-
-    # Calculate the Spearman rank correlation coefficient (rho)
-    numerator = 6 * sum([d ** 2 for d in rank_diff])
-    denominator = n * (n ** 2 - 1)
-    rho = 1 - numerator / denominator
+    rho = 1 - 6 * np.sum(d**2) / (n * (n**2 - 1))
 
     return rho
 
 
-def create_buckets(data, bins=10, mode='uniform'):
+def create_buckets(arr, bins=10, mode='uniform'):
     """
-    Assigns a bucket for each value.
+    Assigns values in an array to buckets based on specified binning criteria.
     """
     if mode == 'uniform':
-        bin_edges = np.linspace(data.min(), data.max(), bins + 1)
+        bin_edges = np.linspace(arr.min(), arr.max(), bins + 1)
     elif mode == 'quntile':
         quantiles = np.linspace(0, 100, bins + 1)
-        bin_edges = np.percentile(a=data, q=quantiles)
+        bin_edges = np.percentile(a=arr, q=quantiles)
     else:
         raise ValueError("Invalid mode")
     
-    bin_indices = np.digitize(x=data, bins=bin_edges)
+    bin_indices = np.digitize(x=arr, bins=bin_edges)
     bin_indices_str = [str(i) for i in bin_indices]
-    
+
     return bin_indices_str
 
 
 def contingency_table(x, y):
     """
-    Creates contingency table out of (x,y) pairs.
+    Create a contingency table from paired variables (x, y).
+
+    The input variables can be a mix of categorical and numerical data. 
+    If the input variable is numerical, is is automatically divided into buckets.
     """
-    assert len(x) == len(y), "Input must be the same length"
+    assert len(x) == len(y), "Input variables must be the same length!"
 
     if all(isinstance(xi, (int, float, np.int64)) for xi in x):
         x = create_buckets(x)
@@ -87,12 +89,14 @@ def contingency_table(x, y):
 
 
 def chi_squared_statistic(table):
+    """
+    Calculate the chi-squared statistic for a given contingency table.
+    """
     row_sums = np.sum(table, axis=1)
     col_sums = np.sum(table, axis=0)
     total_sum = np.sum(table)
     
     chi_squared = 0
-    
     for i in range(table.shape[0]):
         for j in range(table.shape[1]):
             expected = (row_sums[i] * col_sums[j]) / total_sum
@@ -102,6 +106,9 @@ def chi_squared_statistic(table):
 
 
 def phi_k_correlation(x, y):
+    """
+    Calculate the Phi-K correlation coefficient.
+    """
     table = contingency_table(x, y)
     chi_squared = chi_squared_statistic(table)
     
@@ -110,4 +117,5 @@ def phi_k_correlation(x, y):
     r = table.shape[1]
     
     phi_k = np.sqrt(chi_squared / (n * min(k - 1, r - 1)))
+
     return phi_k
